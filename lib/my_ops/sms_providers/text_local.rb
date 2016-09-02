0bfcbb7f8b40ba9b47eaf99065006641a5203389
Adam Cooke <me@adamcooke.io>
2016-09-02 12:10:17 +0100

    actually check for errors from the API and return them as appropriate plus add log file

--- lib/my_ops/sms_providers/text_local.rb
+++ lib/my_ops/sms_providers/text_local.rb
@@ -9,6 +9,7 @@ module MyOps
       self.provider_description = "Send alert SMS messages using the TextLocal service (www.textlocal.co.uk)"
 
       def send_message(recipient, message)
+        self.class.logger.info "Sending to '#{recipient}' with message '#{message}'"
         response = Nifty::Utils::HTTP.post("https://api.txtlocal.com/send/",
           :params => {
             :username => self.class.config.username,
@@ -18,11 +19,27 @@ module MyOps
             :numbers => recipient
           }
         )
-        response[:code] == 200
+
+        if response[:code] == 200
+          if response[:body]["errors"].is_a?(Array) && response[:body]["errors"].size > 0
+            self.class.logger.warn "Couldn't send message to #{recipient}. #{response[:body].inspect}".red
+            false
+          else
+            self.class.logger.debug "Successfully sent message to #{recipient}".green
+            true
+          end
+        else
+          self.class.logger.fatal "Couldn't send message to #{recipient}. API returned #{response[:code]} status.".red
+          false
+        end
       end
 
       def self.config
         MyOps.module_config["myops-textlocal"]
+      end
+
+      def self.logger
+        @logger ||= Logger.new(Rails.root.join('log', 'textlocal.log'))
       end
 
     end
